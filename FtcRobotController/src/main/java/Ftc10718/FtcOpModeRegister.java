59c5df8c188ee53100eee422e1f39ebf2c35b5fb
Trc3543 <trc492@live.com>
2016-01-15 15:09:38 -0800

    COmbined with old code base.

--- FtcRobotController/src/main/java/Ftc10718/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/Ftc10718/FtcOpModeRegister.java
@@ -54,6 +54,8 @@ public class FtcOpModeRegister implements OpModeRegister
          *
          * If two or more op modes are registered with the same name, the app will display an error.
          */
+        manager.register("Autonomous", Autonomous.class);
+        manager.register("DriverMode", DriverMode.class);
         manager.register("FtcAuto", FtcAuto.class);
         manager.register("FtcTeleOp", FtcTeleOp.class);
         manager.register("FtcTest", FtcTest.class);
